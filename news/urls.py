e2588676a45e7bc7298c1fa342af65dbbc031447
pimpy6565 <155408888+pimpy6565@users.noreply.github.com>
2026-01-05 00:31:36 -0500

    Delete news directory

--- news/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('',views.index,name='index'),
-    path('/About',views.About,name="About"),
-    path('/Disclaimer',views.Disclaimer,name='Disclaimer')
-]
