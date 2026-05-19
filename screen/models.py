5a6c694b02153fd782c0fb3b44608a28a87b9e12
christopher <chris@christophers-MacBook-Pro.local>
2026-05-18 20:50:35 -0400

    added polar to code sheet

--- screen/models.py
+++ screen/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Hplc(models.Model):
+    name = models.CharField(max_length=36)
+    benz = models.DecimalField(default=0,max_digits=10,decimal_places=2)
+    caf = models.DecimalField(default=0,max_digits=10,decimal_places=2)
+    sorb = models.DecimalField(default=0,max_digits=10,decimal_places=2)
+    ace = models.DecimalField(default=0,max_digits=10,decimal_places=2)
+    
+    def __str__(self):
+        return self.name.title()
