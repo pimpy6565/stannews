c4842decd81fba741b7ffef2ef64ee2d84f6f893
christopher <chris@christophers-MacBook-Pro.local>
2026-05-18 23:57:11 -0400

    HPLC NEW login required

--- screen/views.py
+++ screen/views.py
@@ -153,6 +153,7 @@ def rando(request):
 def app(request):
     return render(request,"screen/index.html")
 
+@login_required
 def hplc(request):
     hplc_db = Hplc.objects.all()
     return render(request,"screen/hplc.html",{
