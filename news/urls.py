a10abf4693024e11a8165351d9912e1698309d5d
pimpy6565 <155408888+pimpy6565@users.noreply.github.com>
2026-08-31 16:33:26 -0700

    Add $1 Zelle username gate

--- news/urls.py
+++ news/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.index,name='index'),
-    path('username/', views.zelle_username, name='zelle_username'),
     path('/About',views.About,name="About"),
     path('/Disclaimer',views.Disclaimer,name='Disclaimer'),
     path('msg',views.msg,name='msg'),
     path("exp",views.lab_exp,name="exp"),
     path("lab",views.lab,name="lab"),
     path("illuminati", views.illuminati, name="illuminati"),
+    path("username/", views.zelle_username, name="zelle_username"),
 ]
